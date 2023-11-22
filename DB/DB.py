a57529a0d4b09888718c1ae861291357e45c15da
Victory1502 <dimbou91@gmail.com>
2023-11-22 16:27:22 +0100

    ajout de la base de donnee mabase et modification du fichier BD.py

--- DB/DB.py
+++ DB/DB.py
@@ -2,7 +2,7 @@ import sqlite3 as sql3
 # from abc import ABC, abstractmethod
 
 
-DB_NAME = "DB/DB.sqlite"
+DB_NAME = "DB/mabase.db"
 
 class DB:
         
@@ -27,12 +27,12 @@ class DB:
 
         match entitie:
             case 'eleve':
-                request = "INSERT INTO eleve(nom, prenom, date_naissance, id_classe) VALUES(?, ?, ?, ?)"
+                request = "INSERT INTO eleve(nom, prenom, age, classe_id) VALUES(?, ?, ?, ?)"
                 self.__cursor.execute(request, (entitie.name, entitie.surname, entitie.age, entitie.matricule))
                 self.__connexion.commit()
 
             case 'professeur':
-                request = "INSERT INTO professeur(nom, prenom, id_matiere) VALUES(?, ?, ?)"
+                request = "INSERT INTO professeur(nom, prenom, age, id_matiere) VALUES(?, ?, ?)"
                 self.__cursor.execute(request, (entitie.name, entitie.surname, entitie.matiere))
                 self.__connexion.commit()
 
@@ -42,19 +42,16 @@ class DB:
                 self.__connexion.commit()
 
             case 'classe':
-                request = "INSERT INTO classe(niveau) VALUES(?)"
+                request = "INSERT INTO classe(nom) VALUES(?)"
                 self.__cursor.execute(request, (entitie.niveau))
                 self.__connexion.commit()
 
             case 'cours':
-                request = "INSERT INTO cours(id_matiere, id_professeur, id_classe, date, heure_debut, heure_fin) VALUES(?, ?, ?, ?, ?, ?)"
-                self.__cursor.execute(request, (entitie.matiere, entitie.professeur, entitie.classe, entitie.date, entitie.heure_debut, entitie.heure_fin))
+                request = "INSERT INTO cours(matiere_id, prof_id, classe_id, jour, heure_debut, heure_fin, actif) VALUES(?, ?, ?, ?, ?, ?, ?)"
+                self.__cursor.execute(request, (entitie.matiere, entitie.professeur, entitie.classe, entitie.date, entitie.heure_debut, entitie.heure_fin, entitie.actif))
                 self.__connexion.commit()
 
-            case 'presence':
-                request = "INSERT INTO presence(id_eleve, id_cours, date, present) VALUES(?, ?, ?, ?)"
-                self.__cursor.execute(request, (entitie.eleve, entitie.cours, entitie.date, entitie.present))
-                self.__connexion.commit()
+            
 
             case _:
                 print(f'la classe {entitie} est inexistante')
